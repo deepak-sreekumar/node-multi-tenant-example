3aef686f550632da33df30532c20b4251586c0fb
deepak-sreekumar <deepakstark@gmail.com>
2022-10-13 16:57:09 +0530

    removed unused method

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,12 @@
 import express from "express";
 import { getAllUsers } from "./controllers/UserController";
 import { apiTenantContextHandler } from "./middlewares/apiTenantContextHandler";
-import { cleanUp } from "./middlewares/cleanUp";
 
 export const app = express();
 
 app.use(apiTenantContextHandler);
 
 app.get("/users", getAllUsers);
-
-app.use(cleanUp);
 
 app.listen(3000, () => {
   console.log("Listening on 3000");
